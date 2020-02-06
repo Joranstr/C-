3794c30939cced716028350626bb41d7531ba0c3
Joranstr <55440769+Joranstr@users.noreply.github.com>
2020-02-06 15:14:45 +0100

    Create HelpList.cs

---
+++ Oblig/Oblig1/C Sharp Oblig 1/C Sharp Oblig 1/HelpList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Oblig_1
+{
+    class HelpList
+    {
+        private static void Helptext()
+        {
+            Console.WriteLine("comando linjer");
+            Console.WriteLine("help - viser tiljengelige comando linjer.");
+            Console.WriteLine("liste - viser alle personer med id, fornavn, etter navn, fødselsår, dødsår og evt. forerdre å barn til persjoner og dem's id.");
+            Console.WriteLine("vis +id - viser besteme personene med mor far og dem's id");
+        }
+    }
+}
